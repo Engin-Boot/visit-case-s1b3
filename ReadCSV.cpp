228b0336104067afdfdb4972978a656145a6e9f6
Nakul Ramanathan <nakul16168@iiitd.ac.in>
2020-09-15 14:15:25 +0530

    Added comments to ReadCSV.cpp

--- ReadCSV.cpp
+++ ReadCSV.cpp
@@ -1,7 +1,13 @@
 #include "ReadCSV.h"
+// This file contains all the constructors and member functions of the ReadCSV class
 
+// Parameterized constructor which takes two inputs
+// 1. Reference to the string which stores the path to the file
+// 2. Reference to the string which acts as the delimiter in the csv file
 ReadCSV::ReadCSV(const string &file, const string &delm) : fileName(file), delimeter(delm) {}
 
+
+// Member function which reads each row of the csv file as a string and appends it into a vector
 vector<string> ReadCSV::readCSVData()
 {
 
